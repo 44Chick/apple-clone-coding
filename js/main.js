c0c8468454371aff8629c999ff0f6e13d7311b71
sangwonyu <havedreamm@naver.com>
2020-12-13 09:24:39 +0900

    activate current scene

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@
 // 지역 변수로 해준다.
 (() => {
     let yOffset = 0;
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
     const scenInfo = [
         {
             type: 'sticky', // 세션이 고정된 위치
@@ -42,6 +45,7 @@
 
     function setLayout() {
         // 각 스크롤 세션의 높이 셋팅
+
         for (const info of scenInfo) {
             info.scrollheight = info.heightNum * window.innerHeight;
             info.objs.container.style.height = `${info.scrollheight}px`
@@ -51,6 +55,19 @@
 
     function scrollLoop() {
         // 현 스크롤 위치 ( yOffSet ) 과 각 세션의 높이 비교 시작 유무 확인
+        prevScrollHeight = 0; // 초기화
+        for(let i = 0; i < currentScene; i++) {
+            prevScrollHeight += scenInfo[i].scrollheight;
+        }
+
+        if (yOffset > prevScrollHeight + scenInfo[currentScene].scrollheight) {
+            currentScene++;
+        }
+
+        if(yOffset < prevScrollHeight) {
+            if (currentScene === 0) return; // 창 바운스되면서 - 되는 것 방지
+            currentScene--;
+        }
     }
     window.addEventListener('resize', setLayout);
     window.addEventListener('scroll', () => {
