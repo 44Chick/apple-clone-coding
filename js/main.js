876e6b5e897f9ac9613401508cdadece01bc115a
sangwonyu <havedreamm@naver.com>
2020-12-17 07:37:50 +0900

    animation 4

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
     let yOffset = 0;
     let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
     let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
-
+    let enterNewScene = false; // 새로운 scene이 시작되는 순간 true
     const scenInfo = [
         {
             type: 'sticky', // 세션이 고정된 위치
@@ -104,20 +104,26 @@
     function scrollLoop() {
         // 현 스크롤 위치 ( yOffSet ) 과 각 세션의 높이 비교 시작 유무 확인
         prevScrollHeight = 0; // 초기화
+        enterNewScene = false;
+
         for(let i = 0; i < currentScene; i++) {
             prevScrollHeight += scenInfo[i].scrollheight;
         }
 
         if (yOffset > prevScrollHeight + scenInfo[currentScene].scrollheight) {
+            enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
         if(yOffset < prevScrollHeight) {
+            enterNewScene = true;
             if (currentScene === 0) return; // 창 바운스되면서 - 되는 것 방지
             currentScene--;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
+
+        if (enterNewScene) return;
         playAnimation()
     }
     window.addEventListener('resize', setLayout);
