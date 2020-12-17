1c46b4732ec737295cb5fc6d103e4cd0979b81bc
sangwonyu <havedreamm@naver.com>
2020-12-18 07:38:08 +0900

    specific part animation

--- js/main.js
+++ js/main.js
@@ -21,7 +21,8 @@
                 messageD: document.querySelector('#scroll-section-0 .main-message.d'),
             },
             values: {
-                messageA_opacity: [0,1]
+                messageA_opacity: [0,1, {start: 0.1, end: 0.2}],
+                messageB_opacity: [0,1, {start: 0.3, end: 0.4}]
             }
             // 스크롤에 따른 애니메이션 벨류값
             // 스크롤에 따른 변수 : opacity, translate: transfor ( 투명도, y값이동 )
@@ -94,9 +95,29 @@
 
     function calcValues(values, currentYOffset) {
         let rv
+        const scrollHeight = scenInfo[currentScene].scrollheight;
         // 현재 씬(스코롤섹션)에서 스크롤된 범위를비율로 구하기
-        let scrollRatio = currentYOffset / scenInfo[currentScene].scrollheight;
-        rv = scrollRatio * (values[1] - values[0]) + values[0];
+        const scrollRatio = currentYOffset / scrollHeight;
+
+        if (values.length === 3) {
+            // start ~ end 사이에 애니메이션 실행
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            // start end 사이 height
+            const partScrollHeight = partScrollEnd - partScrollStart;
+
+            if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+                rv =  (currentYOffset - partScrollStart) / partScrollHeight * (values[1]  - values[0]);
+            } else if (currentYOffset < partScrollStart) {
+                rv = values[0];
+            } else if (currentYOffset > partScrollEnd) {
+                rv = values[1];
+            }
+
+        } else {
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
+
 
         return rv;
     }
