77c5d0f2381dfe1945a23f4cb229f1274b690da1
sangwonyu <havedreamm@naver.com>
2020-12-28 07:20:01 +0900

    draw canvas before starting scene3

--- js/main.js
+++ js/main.js
@@ -281,6 +281,54 @@
                     objs.pinC.style.transform = `scaleY(${calcValues(values.pinC_scaleY, currentYOffset)})`;
                 }
 
+                // currentScene 3에서 쓰는 캔버스를 미리 그려주기 시작
+                if (scrollRatio > 0.9) {
+                    const objs = scenInfo[3].objs;
+                    const values = scenInfo[3].values;
+                    const widthRatio = window.innerWidth / objs.canvas.width;
+                    const heightRatio = window.innerHeight / objs.canvas.height;
+                    let canvasScaleRatio;
+
+                    if (widthRatio <= heightRatio) {
+                        // 캔버스보다 브라우저 창이 홀쭉한 경우
+                        canvasScaleRatio = heightRatio;
+                    } else {
+                        // 캔버스보다 브라우저 창이 납작한 경우
+                        canvasScaleRatio = widthRatio;
+                    }
+
+                    objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+                    objs.context.fillStyle = 'white';
+                    objs.context.drawImage(objs.images[0], 0, 0);
+
+                    // 캔버스 사이즈에 맞춰 가정한 innerwidth와 innerHeight
+                    // innerWidth -> scroll영역 때문에 계산이 잘 안맞음
+                    // document.body.offsetwidth
+                    const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio;
+                    const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
+
+                    const whiteRectWidth = recalculatedInnerWidth * 0.15;
+                    values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+                    values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+                    values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+                    values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+                    // 좌우 흰색 박스 그리기
+                    // objs.context.fillRect(values.rect1X[0], 0, parseInt(whiteRectWidth), objs.canvas.height)
+                    // objs.context.fillRect(values.rect2X[0], 0, parseInt(whiteRectWidth), objs.canvas.height)
+                    objs.context.fillRect(
+                        Math.round(values.rect1X[0]),
+                        0,
+                        Math.round(whiteRectWidth),
+                        objs.canvas.height
+                    );
+                    objs.context.fillRect(
+                        Math.round(values.rect2X[0]),
+                        0,
+                        Math.round(whiteRectWidth),
+                        objs.canvas.height
+                    );
+                }
                 break;
             case 3:
                 // 가로/세로 모두 꽉 차게 하기 위해 여기서 셋팅(계산 필요)
